0e276c67d5e9337531d723d8c811a997cc03b051
L33t-Rose <jphillipjean@gmail.com>
2023-01-03 16:49:52 -0500

    [fix]: Fixed load so that it doesn't update the store on every request

--- src/routes/dex/[pokemon]/+page.server.ts
+++ src/routes/dex/[pokemon]/+page.server.ts
@@ -2,6 +2,20 @@ import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from "./$types";
 import { PokemonStore, type Pokemon } from "$lib/util";
 
+async function addPokemonToCache(name:string){
+   console.log("fetching to the api")
+   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+   const data = await res.json() as Pokemon;
+
+   PokemonStore.update((store)=> {
+      store.set(name,data);
+      return store;
+   })
+
+   return data;
+}
+
+
 export const load = (async ({ params }) => {
    if (!params.pokemon) {
       throw redirect(301,"/dex");
@@ -10,14 +24,11 @@ export const load = (async ({ params }) => {
    const name = params.pokemon.toLowerCase();
    const unsub = PokemonStore.subscribe((data) => {
       if (data.has(name)) {
+         console.log("Getting data from our cache")
          res = data.get(name)!;
       }
    });
-   res ??= await (await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)).json() as Pokemon;
-   PokemonStore.update((store)=> {
-      store.set(name,res);
-      return store;
-   })
+   res ??= await addPokemonToCache("name");
    unsub();
    return res;
 }) satisfies PageServerLoad;
